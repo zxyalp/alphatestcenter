3dd1fe958aaf42ce2f92d0a57254ebe21958c37c
zxy <orcexzy@163.com>
2018-08-16 15:33:30 +0800

    单元测试

--- dubbo-unittest-center/src/main/java/com/zxy/utils/MathCalcUtils.java
+++ dubbo-unittest-center/src/main/java/com/zxy/utils/MathCalcUtils.java
@@ -14,21 +14,182 @@ public class MathCalcUtils {
     /**
      * BigDecimal的加法运算方法
      */
-//    public static BigDecimal add(){
-//
-//    }
+    public static BigDecimal add(BigDecimal b1, BigDecimal... bn){
 
+        BigDecimal s = Optional.ofNullable(b1).orElse(BigDecimal.ZERO);
+
+        if (null != bn) {
+            for(BigDecimal b : bn){
+                s = s.add(Optional.ofNullable(b).orElse(BigDecimal.ZERO));
+            }
+        }
+        return s;
+    }
+
+
+    public static Integer add(Integer b1, Integer... bn){
+
+        Integer s = Optional.ofNullable(b1).orElse(0);
+
+        if (null != bn){
+            for (Integer b: bn){
+                s += Optional.ofNullable(b).orElse(0);
+            }
+        }
+        return s > 0 ? s : 0;
+    }
+
+    public static <T extends Number> BigDecimal add(T b1, T... bn){
+        BigDecimal s = Optional.of(BigDecimal.valueOf(b1.doubleValue())).orElse(BigDecimal.ZERO);
+
+        if(null != bn){
+            for (T b: bn){
+                s=s.add(BigDecimal.valueOf(b.doubleValue()));
+            }
+        }
+        return s;
+    }
+
+    /**
+     * BigDecimal的加法运算方法
+     */
+    public static BigDecimal add(String b1, BigDecimal... bn){
+
+        BigDecimal s = Optional.of(new BigDecimal(b1)).orElse(BigDecimal.ZERO);
+
+        if (null != bn) {
+            for(BigDecimal b : bn){
+                s = s.add(Optional.ofNullable(b).orElse(BigDecimal.ZERO));
+            }
+        }
+        return s;
+    }
+
+    /**
+     *  BigDecimal减法运算,减法结果为负数时，默认返回0
+     * @param b1
+     * @param bn
+     * @return
+     */
+
+    public static BigDecimal sub(BigDecimal b1, BigDecimal bn){
+        return sub(true, b1, bn);
+    }
+
+    /**
+     * 减法运算
+     * @param isZero
+     * @param b1
+     * @param bn
+     * @return
+     */
+    public static BigDecimal sub(Boolean isZero, BigDecimal b1, BigDecimal... bn){
+        BigDecimal s = Optional.ofNullable(b1).orElse(BigDecimal.ZERO);
+
+        if(null != bn){
+            for (BigDecimal b: bn){
+                s = s.subtract(Optional.ofNullable(b).orElse(BigDecimal.ZERO));
+            }
+        }
+        return isZero?(s.compareTo(BigDecimal.ZERO) < 0 ?BigDecimal.ZERO:s):s;
+    }
+
+
+    /**
+     * 除法运算，返回2位小数，除数或是被除数为0，返回0.
+     * @param b1
+     * @param b2
+     * @param <T>
+     * @return
+     */
+    public static <T extends Number> BigDecimal div(T b1, T b2){
+        return div(b1, b2, BigDecimal.ZERO);
+    }
+
+
+    /**
+     * BigDecimal的除法封装， 如果除数或是被除数为0，返回默认值
+     * @param b1
+     * @param b2
+     * @param defaultValue
+     * @param <T>
+     * @return
+     */
+    public static <T extends Number> BigDecimal div(T b1, T b2, BigDecimal defaultValue){
+        if( null == b1 || null == b2){
+            return defaultValue;
+        }
+
+        try{
+            return BigDecimal.valueOf(b1.doubleValue()).divide(BigDecimal.valueOf(b2.doubleValue()), 2, BigDecimal.ROUND_DOWN);
+        }catch (Exception e){
+            return defaultValue;
+        }
+    }
+
+
+    /**
+     * 乘法计算
+     * @param b1
+     * @param bn
+     * @param <T>
+     * @return
+     */
+    public static <T extends Number> BigDecimal multiply(T b1, T... bn){
+        if (null == b1|| null == bn){
+            return BigDecimal.ZERO;
+        }
+
+        BigDecimal s = BigDecimal.valueOf(b1.doubleValue());
+
+        for (T b: bn){
+            s = s.multiply(BigDecimal.valueOf(b.doubleValue()));
+        }
+
+        return s.setScale(2, BigDecimal.ROUND_DOWN);
+    }
+
+
+    /**
+     * 乘法计算
+     * @param b1
+     * @param bn
+     * @return
+     */
+    public static BigDecimal multiply(String b1, String... bn){
+        if(null == b1 || null == bn){
+            return BigDecimal.ZERO;
+        }
+
+        BigDecimal s = new BigDecimal(b1);
+
+        for(String b: bn){
+            s = s.multiply(new BigDecimal(b));
+        }
+        return s.setScale(2, BigDecimal.ROUND_DOWN);
+
+    }
 
     public static void main(String[] args) {
         BigDecimal bigDecimal01 = new BigDecimal("200");
         BigDecimal bigDecimal02 = new BigDecimal("200");
+        BigDecimal bigDecimal03 = new BigDecimal("200");
+
+        Double a1 = 100.22;
+        Double b1 = 200.22;
+        Double b2 = 0.00;
 
 
-        BigDecimal bigDecimal= new BigDecimal("200");
+        System.out.println(multiply(a1, b1, b2));
 
-        Optional<BigDecimal> optional = Optional.ofNullable(bigDecimal);
+        System.out.println(add(bigDecimal01, bigDecimal02));
 
-        System.out.println(optional.map(bigDecimal1 -> bigDecimal1.multiply(bigDecimal01)).map(bigDecimal1 -> bigDecimal1.multiply(bigDecimal02)).orElse(BigDecimal.ZERO));
+        int a = 2000;
+        int b = 3000;
+
+        System.out.println(add(a,b));
+
+        System.out.println(bigDecimal01.multiply(bigDecimal02));
 
     }
 
--- dubbo-unittest-center/src/test/java/com/zxy/high/orders/trade/SubsOrPurWebFacadeTest.java
+++ dubbo-unittest-center/src/test/java/com/zxy/high/orders/trade/SubsOrPurWebFacadeTest.java
@@ -4,6 +4,7 @@ package com.zxy.high.orders.trade;
 import com.alibaba.fastjson.JSONObject;
 import com.zxy.high.orders.BaseTestCase;
 import com.zxy.utils.FileExtUtils;
+import com.zxy.utils.MathCalcUtils;
 import com.zxy.utils.RandomExtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.slf4j.Logger;
@@ -16,6 +17,7 @@ import org.testng.annotations.*;
 import org.testng.Assert;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import com.alibaba.fastjson.JSON;
 
@@ -23,6 +25,7 @@ import com.alibaba.fastjson.JSON;
 /**
  * 高端网站下单接口
  * 接口路径：com.howbuy.tms.high.orders.facade.trade.subsorpur.subsorpurweb.SubsOrPurWebFacade;
+ *
  * @author yang.zhou
  * @date 2018/8/14
  */
@@ -30,11 +33,11 @@ public class SubsOrPurWebFacadeTest extends BaseTestCase {
 
     private static final Logger logger = LoggerFactory.getLogger(SubsOrPurWebFacadeTest.class.getName());
 
-    private static final Double FEE_RATE = 0.015;
+    private static final String FEE_RATE = "0.015";
 
-    private static final Double DISCOUNT = 0.9;
+    private static final String DISCOUNT = "0.9";
 
-    private static final Double ACTI_DISCOUNT = 0.5;
+    private static final String ACTI_DISCOUNT = "0.5";
 
     private JSONObject jsonObject;
 
@@ -45,7 +48,7 @@ public class SubsOrPurWebFacadeTest extends BaseTestCase {
     @BeforeMethod
     public void setUp() throws IOException {
 
-        logger.info("开始执行本单元测试.");
+        logger.info(">>>>>>开始执行单元测试方法");
 
         String src = FileExtUtils.readFileToString("classpath:high/orders/trade/SubsOrPurWebRequest.json");
 
@@ -62,11 +65,11 @@ public class SubsOrPurWebFacadeTest extends BaseTestCase {
 
     @AfterMethod
     public void tearDown() throws Exception {
-        logger.info("单元测试结束.");
+        logger.info(">>>>>>单元测试结束");
     }
 
     @Test(enabled = true, description = "高端网站下单场景")
-    public void webFacadeTest() throws Exception {
+    public void web_facade_test() throws Exception {
 
         logger.info("json：{}", JSON.toJSON(jsonObject.toJSONString()).toString());
 
@@ -88,11 +91,73 @@ public class SubsOrPurWebFacadeTest extends BaseTestCase {
 
     }
 
-    @Test(enabled = true, description = "高端产品极差校验失败")
-    public void webFacadeTest01() throws Exception {
 
-        // appAmt=2331050.00
-        jsonObject.put("appAmt", 2500000);
+    /**
+     * 产品购买金额低于产品最低追加申请金额
+     * <p>
+     * 产品CS0501 产品极差：100,追加净购买金额：1000，最低追加购买金额：10000
+     *
+     * @throws Exception
+     */
+    @Test(enabled = true, description = "下单追加购买金额小于产品最低追加申请金额")
+    public void appAmt_less_fund_limit_test() throws Exception {
+
+
+        String purchaseAmt = "9000";
+
+        BigDecimal esitmateFee = MathCalcUtils.multiply(purchaseAmt, FEE_RATE, DISCOUNT);
+
+        BigDecimal appAmt = MathCalcUtils.add(purchaseAmt, esitmateFee);
+
+        logger.info("申请金额{},手续费{}", appAmt, esitmateFee);
+
+        jsonObject.put("appAmt", appAmt);
+        jsonObject.put("esitmateFee", esitmateFee);
+
+        logger.info("json：{}", JSON.toJSON(jsonObject.toJSONString()).toString());
+
+        SubsOrPurWebRequest request = JSON.parseObject(jsonObject.toJSONString(), SubsOrPurWebRequest.class);
+
+        logger.info("请求内容：{}", JSON.toJSON(request).toString());
+
+        SubsOrPurWebResponse response = subsOrPurWebFacade.execute(request);
+
+        logger.info("返回结果：{}", JSON.toJSONString(response));
+
+        if ("Z3000063".equals(response.getReturnCode())) {
+            logger.info("测试通过,{}:{}", response.getReturnCode(), response.getDescription());
+        } else {
+            logger.error("测试失败,{}:{}", response.getReturnCode(), response.getDescription());
+        }
+
+        Assert.assertEquals(response.getReturnCode(), "Z3000027", "交易金额小于追加认申购下限验证失败.");
+
+    }
+
+
+    /**
+     * 产品购买极差校验
+     * <p>
+     * 产品极差计算公式：
+     * （追加净购买金额 - 追加购买金额下限）% 产品极差（prod_differ）求模， 能整除，满足极差，否则极差校验失败。
+     * <p>
+     * 产品CS0501 产品极差：100,追加净购买金额：1000，最低追加购买金额：10000
+     *
+     * @throws Exception
+     */
+    @Test(enabled = true, description = "高端产品极差校验失败")
+    public void fund_range_test() throws Exception {
+
+        String purchaseAmt = "10090";
+
+        BigDecimal esitmateFee = MathCalcUtils.multiply(purchaseAmt, FEE_RATE, DISCOUNT);
+
+        BigDecimal appAmt = MathCalcUtils.add(purchaseAmt, esitmateFee);
+
+        logger.info("申请金额{},手续费{}", appAmt, esitmateFee);
+
+        jsonObject.put("appAmt", appAmt);
+        jsonObject.put("esitmateFee", esitmateFee);
 
         logger.info("json：{}", JSON.toJSON(jsonObject.toJSONString()).toString());
 
@@ -113,5 +178,4 @@ public class SubsOrPurWebFacadeTest extends BaseTestCase {
         Assert.assertEquals(response.getReturnCode(), "Z3000063", "高端产品极差校验通过.");
 
     }
-
 }
